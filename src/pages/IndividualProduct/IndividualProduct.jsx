5e6ec51dd42742ed4d1454da5b650caab9583b93
subham0269 <gitsubham21@gmail.com>
2025-07-30 01:35:44 +0530

    pre final completion update

--- src/pages/IndividualProduct/IndividualProduct.jsx
+++ src/pages/IndividualProduct/IndividualProduct.jsx
@@ -5,17 +5,85 @@ import WrapperContainer from "../../components/Wrapper/WrapperContainer";
 import Heading from "../../components/Heading/Heading";
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
 import css from "./IndividualProduct.module.css";
-import Slider from "react-slick";
 import allListings from "../../data/products/individual_product_listings.json";
 import { useLocation } from "react-router";
 import Loading from "../../components/Loading/Loading";
-import { PiOnigiriFill } from "react-icons/pi";
 import classNames from "classnames";
 import CustomButton from "../../components/Buttons/Buttons";
 import SliderCarousel from "../../components/Slider/Slider";
+import { BsPaperclip } from "react-icons/bs";
+import { CgClose } from "react-icons/cg";
+
+function BuyModal(props) {
+  const { isOpen, onClose, title } = props;
+  useEffect(() => {
+    if (isOpen) {
+      document.getElementById("root").style.overflow = "hidden";
+    } else {
+      document.getElementById("root").style.overflow = "";
+    }
+  }, [isOpen]);
+  return (
+    <div className={classNames(css.overlay, { [css.open]: isOpen })}>
+      <div
+        className={classNames(
+          css.modalContainer,
+          isOpen ? css.animateSlideIn : css.animateSlideOut
+        )}
+      >
+        <CgClose onClick={onClose} className={css.closeBtn} />
+        <Heading level="2" className={css.modalTopHeading}>
+          Customize Your <span>product</span>
+        </Heading>
+        <Heading level="3" className={css.modalProdTitle}>
+          {title}
+        </Heading>
+        <div className={css.formContainer}>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <label for="fullName">
+              <span>Full Name</span>
+              <input name="fullName" type="text" placeholder="Jane Doe" />
+            </label>
+            <label for="mobileNumber">
+              <span>Mobile Number</span>
+              <input
+                name="mobileNumber"
+                type="text"
+                placeholder="+91 98765 43210"
+              />
+            </label>
+            <label for="customizationDetails">
+              <span>Customisation Details</span>
+              <textarea
+                rows={7}
+                name="customizationDetails"
+                // type="text"
+                placeholder="Tell me how you want your product to be customized."
+              />
+            </label>
+            <label className={css.forImageLabel}>
+              <input
+                name="imageUpload"
+                type="file"
+                accept="image/*"
+                className={css.hidden}
+                multiple
+              />
+              <BsPaperclip />
+            </label>
+            <CustomButton className={css.modalFormSubmit} primary outward>
+              Submit
+            </CustomButton>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function IndividualProduct() {
   const [currListing, setCurrentListing] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
   const location = useLocation();
   const productUrl = location.pathname.split("/").pop();
   let average = null;
@@ -45,66 +113,80 @@ function IndividualProduct() {
   }
 
   return (
-    <MainContainer className={css.main}>
-      <Breadcrumbs className={css.changedW} />
-      <Section className={css.sec}>
-        <WrapperContainer>
-          <div className={css.container}>
-            <div className={css.carouselContainer}>
-              <SliderCarousel slides={currListing?.images} />
-            </div>
-            {/* <div className={css.slider}> */}
-            {/* </div> */}
-            <div className={css.mainContentContainer}>
-              <Heading level="1" className={css.heading}>
-                {currListing?.title}
-              </Heading>
-              <div className={css.prodCont}>
-                <div>
-                  <Heading className={css.desHeading} level="2">
-                    Craft Story
-                  </Heading>
-                  <p className={css.desc}>{currListing?.desc}</p>
-                </div>
-                <div>
-                  {currListing?.specialOffer && (
-                    <Heading className={css.specialOffer} level="2">
-                      Special Price
+    <>
+      <MainContainer className={css.main}>
+        <Breadcrumbs className={css.changedW} />
+        <Section className={css.sec}>
+          <WrapperContainer>
+            <div className={css.container}>
+              <div className={css.carouselContainer}>
+                <SliderCarousel slides={currListing?.images} />
+              </div>
+              {/* <div className={css.slider}> */}
+              {/* </div> */}
+              <div className={css.mainContentContainer}>
+                <Heading level="1" className={css.heading}>
+                  {currListing?.title}
+                </Heading>
+                <div className={css.prodCont}>
+                  <div>
+                    <Heading className={css.desHeading} level="2">
+                      Craft Story
                     </Heading>
-                  )}
-                  <div className={css.pricingCont}>
-                    {discounted && <span>{`\u20B9 ${discounted}`}</span>}
-                    <span
-                      className={classNames(css.strike, {
-                        [css.main]: !discounted,
-                      })}
-                    >{`\u20B9 ${original}`}</span>
-                    {average && (
-                      <span className={css.percentage}>{`${average}%`}</span>
-                    )}
+                    <p className={css.desc}>{currListing?.desc}</p>
                   </div>
+                  <div>
+                    {currListing?.specialOffer && (
+                      <Heading className={css.specialOffer} level="2">
+                        Special Price
+                      </Heading>
+                    )}
+                    <div className={css.pricingCont}>
+                      {discounted && <span>{`\u20B9 ${discounted}`}</span>}
+                      <span
+                        className={classNames(css.strike, {
+                          [css.main]: !discounted,
+                        })}
+                      >{`\u20B9 ${original}`}</span>
+                      {average && (
+                        <span className={css.percentage}>{`${average}%`}</span>
+                      )}
+                    </div>
+                  </div>
+                  <div>
+                    <Heading className={css.delivery} level="2">
+                      Tentative Shopping Time
+                    </Heading>
+                    <p className={css.charges}>{`${min} - ${max} ${unit}`}</p>
+                  </div>
+                  <div>
+                    <Heading className={css.delivery} level="2">
+                      Extra Charges
+                    </Heading>
+                    <ExtraChargesInfo
+                      extraCharges={currListing?.extraCharges}
+                    />
+                  </div>
+                  <CustomButton
+                    onClick={() => setIsOpen(true)}
+                    outward={"true"}
+                    className={css.button}
+                    primary
+                  >
+                    Buy Now
+                  </CustomButton>
                 </div>
-                <div>
-                  <Heading className={css.delivery} level="2">
-                    Tentative Shopping Time
-                  </Heading>
-                  <p className={css.charges}>{`${min} - ${max} ${unit}`}</p>
-                </div>
-                <div>
-                  <Heading className={css.delivery} level="2">
-                    Extra Charges
-                  </Heading>
-                  <ExtraChargesInfo extraCharges={currListing?.extraCharges} />
-                </div>
-                <CustomButton outward={"true"} className={css.button} primary>
-                  Buy Now
-                </CustomButton>
               </div>
             </div>
-          </div>
-        </WrapperContainer>
-      </Section>
-    </MainContainer>
+          </WrapperContainer>
+        </Section>
+      </MainContainer>
+      <BuyModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        title={currListing?.title}
+      />
+    </>
   );
 }
 
